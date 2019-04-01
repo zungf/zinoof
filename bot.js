9018bcf688b68b97be33c0ceda6bb8574ea1c949
Dev <41270843+zungf@users.noreply.github.com>
2019-04-01 20:02:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,6 +53,30 @@ if (message.content.startsWith(adminprefix + 'twitch')) {
     });
 
 
+client.on("message", message => {
+    var prefix = "-";
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **لا يوجد لديك صلاحية لمسح الشات**');
+        var msg;
+        msg = parseInt();
+     
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done | تــم مسح الشات",
+        color: 0x06DF00,
+        description: "تم مسح الرسائل ",
+        footer: {
+          text: "Premuim"
+        }
+      }}).then(msg => {msg.delete(1000)});
+                          }
+ 
+     
+});
+	
+
 
 
 
