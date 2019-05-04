4e8d838c8357d0a285306cfda920756168449514
Dev <41270843+zungf@users.noreply.github.com>
2019-05-04 23:45:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,31 +26,64 @@ const sql = require("sqlite");
 const sWlc = {}
 
 const adminprefix = "z";
-const devs = ['256447238698434562' , '256447238698434562' , ''];
-client.on('message', message => {//for dev
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
 
-if (message.content.startsWith(adminprefix + 'playing')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+
+
+const developers = ['536009907120504847'];
+
+client.on('message', message => {//Toxic Codes
+    var argresult = message.content.split(` `).slice(1).join(' ');//Toxic Codes
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(prefix + 'setgame')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✽ Changed the Bot PLAYING to » __${argresult}__**.`)
+  } else 
+     if (message.content === (prefix + "leave")) {//Toxic Codes
+    message.guild.leave();   //Toxic Codes
+  } else  
+  if (message.content.startsWith(prefix + 'setwatch')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✽ Changed the Bot WATCHING to » __${argresult}__**.`)
+  } else 
+  if (message.content.startsWith(prefix + 'setlisten')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✽ Changed the Bot LISTENING to » __${argresult}__**.`)
+  } else 
+  if (message.content.startsWith(prefix + 'setstream')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/imd3s_x");
+      message.channel.send(`**✽ Changed the Bot STREAMING to » __${argresult}__**.`)
+  }
+  if (message.content.startsWith(prefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`**✽ Changed the Bot NAME to » __${argresult}__**.`)
 } else
-  if (message.content.startsWith(adminprefix + 'name')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+  if (message.content.startsWith(prefix + 'setprefix1')) {
+  client.user.setPrefix(argresult).then
+      message.channel.send(`**✽ Changed the Bot PREFIX to » __${argresult}__**.`)
 } else
-  if (message.content.startsWith(adminprefix + 'avatar')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-      } else
-if (message.content.startsWith(adminprefix + 'twitch')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/alawy");
-    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+if (message.content.startsWith(prefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`**✽ Changed the Bot AVATAR to » __${argresult}__**.`);
 }
+});
 
-
-    });
+client.on('message', message => {  
+let CH = client.guilds.get("501728913710645260").emojis.find(r => r.name === "check");
+let CR = client.guilds.get("501728913710645260").emojis.find(r => r.name === "cross");
+    if (message.author.bot) return;
+if (message.content.startsWith(prefix + 'clear')) { //Codes
+    if(!message.channel.guild) return message.reply('**${CR}| This Command For Servers Only!**'); 
+        if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(`**${CR} | You don't have Premissions!**`);
+        if(!message.guild.member(client.user).hasPermission('MANAGE_MESSAGES')) return message.channel.send(`**${CR} | I don't have Permission!**`);
+ let args = message.content.split(" ").slice(1)
+    let messagecount = parseInt(args);
+    if (args > 100) return message.reply(`**${CR} | The number can't be more than **100** .**`).then(messages => messages.delete(5000))
+    if(!messagecount) args = '100';
+    message.channel.fetchMessages({limit: messagecount + 1}).then(messages => message.channel.bulkDelete(messages));
+    message.channel.send(`**${CH} | Done , Deleted \`${args}\` messages.**`).then(messages => messages.delete(5000));
+  }
+  });
 
 
 
